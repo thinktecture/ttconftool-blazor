2f5051131696d64f598aa76ba415c4fbd3d4213e
Christian Weyer <christian.weyer@thinktecture.com>
2021-03-08 13:06:23 +0100

    Working with enums

---
+++ Shared/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace TTConfTool.Shared
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum value)
+        {
+            var member = value.GetType().GetMember(value.ToString())[0];
+            var displayAttribute = member.GetCustomAttribute<DisplayAttribute>();
+
+            if (displayAttribute != null)
+                return displayAttribute.GetName();
+
+            return value.ToString();
+        }
+    }
+}
