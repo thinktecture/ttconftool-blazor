9171101f980e6481f20e565038ca1ae5fd6c464b
Christian Weyer <christian.weyer@thinktecture.com>
2021-03-08 20:29:27 +0100

    Bug fixes. Adding works now

--- Shared/DTO/Conference.cs
+++ Shared/DTO/Conference.cs
@@ -1,19 +1,26 @@
 ﻿using System;
+using System.Text.Json.Serialization;
+using TTConfTool.Shared.Json;
 
 namespace TTConfTool.Shared.DTO
 {
     public class Conference
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)] 
         public long ID { get; set; }
         public string Title { get; set; }
+        [JsonConverter(typeof(ConfToolDateTimeOffsetConverter))] 
         public DateTime? DateFrom { get; set; }
+        [JsonConverter(typeof(ConfToolDateTimeOffsetConverter))] 
         public DateTime? DateTo { get; set; }
         public Country Country { get; set; }
         public string City { get; set; }
         public string Url { get; set; }
         public int ContributionsCount { get; set; }
         public int SpeakerCount { get; set; }
+        [JsonConverter(typeof(ConfToolDateTimeOffsetConverter))] 
         public DateTime? CfpStart { get; set; }
+        [JsonConverter(typeof(ConfToolDateTimeOffsetConverter))] 
         public DateTime? CfpDeadline { get; set; }
     }
 }
--- Shared/Json/ConfToolDateTimeOffsetConverter.cs
+++ Shared/Json/ConfToolDateTimeOffsetConverter.cs
@@ -5,24 +5,48 @@ using System.Text.Json.Serialization;
 
 namespace TTConfTool.Shared.Json
 {
-    public class ConfToolDateTimeOffsetConverter : JsonConverter<DateTime>
+    public class ConfToolDateTimeOffsetConverter : JsonConverter<DateTime?>
     {
         private const string _dateFormat1 = "yyyy-MM-dd HH:mm:ss";
         private const string _dateFormat2 = "yyyy-MM-dd HH:mm:ss.fffffff";
+        private const string _dateFormat3 = "yyyy-MM-dd'T'HH:mm:ss.fff'Z'";
 
-        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override bool HandleNull => true;
+
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
+            if(reader.GetString() is null)
+            {
+                return null;
+            }
+
             try
             {
                 return DateTime.ParseExact(reader.GetString(), _dateFormat1, CultureInfo.InvariantCulture);
             }
             catch
             {
-                return DateTime.ParseExact(reader.GetString(), _dateFormat2, CultureInfo.InvariantCulture);
+                try
+                {
+                    return DateTime.ParseExact(reader.GetString(), _dateFormat2, CultureInfo.InvariantCulture);
+                }
+                catch (Exception)
+                {
+                    return DateTime.Parse(reader.GetString(), CultureInfo.InvariantCulture);
+                }
             }
         }
 
-        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options) =>
-            writer.WriteStringValue(value.ToString(_dateFormat1, CultureInfo.InvariantCulture));
+        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
+        {
+            if(value is null)
+            {
+                writer.WriteStringValue(string.Empty);
+            }
+            else
+            {
+                writer.WriteStringValue(value.Value.ToString(_dateFormat3, CultureInfo.InvariantCulture));
+            }
+        }
     }
 }
