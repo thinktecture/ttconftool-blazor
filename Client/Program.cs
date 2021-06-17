e6fd785bcfb1bd24aea08258a2a828950d8c03d0
Christian Weyer <christian.weyer@thinktecture.com>
2021-06-17 17:06:43 +0200

    Some tweaks - runtime error!

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
 using TTConfTool.Shared;
 using TTConfTool.UI;
